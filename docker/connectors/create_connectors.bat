dfa3eec08b222bfa3c8adf4f4b13ed5fa3777a10
Vojtěch Danišík <voj.dan@seznam.cz>
2022-05-09 19:18:45 +0200

    nová verze mysql.

--- docker/connectors/create_connectors.bat
+++
@@ -1,2 +0,0 @@
-curl http://localhost:28082/connectors -H "Content-type:application/json" -X POST -d @elasticsearch_sink_connector.txt
-curl http://localhost:28082/connectors -H "Content-type:application/json" -X POST -d @mongo_source_connector.txt
